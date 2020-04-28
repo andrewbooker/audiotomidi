26f64918ce40195ba19e28017b873b47b9d5d04d
Andrew Booker <improvizone@gmail.com>
2020-04-28 10:03:46 +0100

    cutting unused stuff

--- exampleWaveAnalysis.py
+++ exampleWaveAnalysis.py
@@ -4,7 +4,7 @@ import sys
 import soundfile as sf
 import os
 import json
-from utils.analysis import *
+from utils.analysis import AbsMovingAvg, Threshold, SchmittHysteresis
 
 infile = sys.argv[1]
 workingDir = os.path.dirname(infile)
@@ -19,31 +19,25 @@ allData = {}
 allData["original"] = [float(d) for d in data]
 
 avgSml = AbsMovingAvg(44)
-avgLrg = AbsMovingAvg(441)
 thr = Threshold(lambda: 0.1)
 hyst = SchmittHysteresis(lambda: 0.08, lambda: 0.01)
 
 avgSmlValues = []
-avgLrgValues = []
 thrValues = []
 hystValues = []
 
 def proc(d):
     avgSml.add(d)
-    avgLrg.add(d)
     
     thr.add(avgSml.value())
     hyst.add(avgSml.value())
 
     avgSmlValues.append(avgSml.value())
-    avgLrgValues.append(avgLrg.value())
     thrValues.append(thr.value() * 0.2)
     hystValues.append(hyst.value() * 0.25)
 
 [proc(float(d)) for d in data]
 
-#allData["movingAvgSmall"] = avgSmlValues
-#allData["movingAvgLarge"] = avgLrgValues
 allData["movingAvgSmall->threshold"] = thrValues
 allData["movingAvgSmall->SchmittHysteresis"] = hystValues
 
