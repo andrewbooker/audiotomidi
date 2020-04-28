5a204a6e71cb67c3a93e6e43ba9c15a49edac958
Andrew Booker <improvizone@gmail.com>
2020-04-28 23:09:06 +0100

    hooking up audio chaser to MIDI out and issuing random notes for demo

---
+++ README.md
@@ -0,0 +1,5 @@
+# mediautils
+
+requires mediautils project in adjacent direcory:
+
+git clone https://github.com/andrewbooker/mediautils.git
--- chaseAudio.py
+++ chaseAudio.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 
-import sounddevice as sd
-from utils.analysis import AbsMovingAvg, Threshold
+import sys
+import os
+sys.path.append(os.path.dirname(os.getcwd()))
+from mediautils.usbdevices import UsbMidiDevices, MidiOut
 
-
-device = 7
-import time
-
-
+import random
 class Consumer():
+    def __init__(self, midiOut):
+        self.midiOut = midiOut
+        self.note = 64
+        self.velocity = 99
+
     def on(self):
-        print("on")
+        self.midiOut.note_on(self.note, self.velocity, 0)
 
     def off(self):
-        print("off")
+        self.midiOut.note_off(self.note, 0, 0)
+        self.note = random.randint(48, 79)
         
-
+from utils.analysis import AbsMovingAvg, Threshold
 class Chaser():
     def __init__(self, consumer):
         self.avg = AbsMovingAvg(44)
@@ -39,15 +43,42 @@ class Chaser():
     def callback(self):
         return lambda indata, frames, t, status: [self._add(v[0]) for v in indata]
     
-consumer = Consumer()
+import time
+import sounddevice as sd
+def audioCapture(device, shouldStop):
+    print("starting audio capture on %s" % device)
+    with sd.InputStream(samplerate=44100.0, device=device, channels=1, callback=chaser.callback(), blocksize=44) as stream:
+        stream.start()
+        while not shouldStop.is_set():
+            time.sleep(1)
+    
+
+midiDevices = UsbMidiDevices()
+midiOut = MidiOut(midiDevices)
+consumer = Consumer(midiOut.io)
 chaser = Chaser(consumer)
 
+import readchar
+import threading
 
-print("starting audio capture on %s" % device)
-with sd.InputStream(samplerate=44100.0, device=device, channels=1, callback=chaser.callback(), blocksize=44) as stream:
-    time.sleep(1)
-    stream.start()
-    while True:
-        time.sleep(0.1)
+shouldStop = threading.Event()
+thread = threading.Thread(target=audioCapture, args=(7,shouldStop,), daemon=True)
+thread.start()
+
+print("press 'q' to exit")
+while not shouldStop.is_set():
+    c = readchar.readchar()
+    if c == "q":
+        shouldStop.set()
+        thread.join()
+
+del midiOut
+del midiDevices
+print("done")
+
+
+
+        
+    
 
 
