982b64b35da47060ecf585ac78db96ad941a2656
Andrew Booker <improvizone@gmail.com>
2020-05-07 19:21:46 +0100

    selecting first available device using USB audio lookup librabry and sending all notes off on exit

--- chaseAudio.py
+++ chaseAudio.py
@@ -13,6 +13,7 @@ def checkImport(lib):
 
 checkImport("mediautils")
 from mediautils.mididevices import UsbMidiDevices, MidiOut
+from mediautils.audiodevices import UsbAudioDevices
 
 checkImport("compositionutils")
 from compositionutils.scale import Scale, Modes
@@ -41,12 +42,12 @@ class Consumer():
 import time
 import sounddevice as sd
 def audioCapture(device, shouldStop, callback):
-    print("starting audio capture on %s" % device)
+    print("starting audio capture")
     with sd.InputStream(samplerate=44100.0, device=device, channels=1, callback=callback, blocksize=44) as stream:
         stream.start()
         while not shouldStop.is_set():
             time.sleep(1)
-    
+
 
 midiDevices = UsbMidiDevices()
 midiOut = MidiOut(midiDevices)
@@ -54,11 +55,15 @@ consumer = Consumer(midiOut.io)
 chaser = Chaser(consumer, 0.04, 0.0039)
 callback = lambda indata, frames, t, status: [chaser.add(v[0]) for v in indata]
 
+devices = UsbAudioDevices()
+audioDevice = [k for k in devices.keys()][0]
+print("using %s" % devices[audioDevice])
+
 import readchar
 import threading
 
 shouldStop = threading.Event()
-thread = threading.Thread(target=audioCapture, args=(7,shouldStop,callback,), daemon=True)
+thread = threading.Thread(target=audioCapture, args=(audioDevice,shouldStop,callback,), daemon=True)
 thread.start()
 
 print("press 'q' to exit")
@@ -68,6 +73,8 @@ while not shouldStop.is_set():
         print("stopping...")
         shouldStop.set()
         thread.join()
+
+midiOut.io.write_short(0xB0, 0x7B, 0)
 
 del midiOut
 del midiDevices
